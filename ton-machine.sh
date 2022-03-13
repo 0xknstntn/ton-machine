9dfa15c36d5091713b4671d36b6efef8c69285a7
mnstr.asuoki <konstantinjogotsen@gmail.com>
2022-03-13 21:11:22 +0500

    Update ton-machine.sh

--- ton-machine.sh
+++ ton-machine.sh
@@ -5,26 +5,27 @@ then
 
 	echo
 	echo "Use the 'help' option for information"
-	exit 45
+	echo
+	cmd /k
 fi
  
-if [ $1 == help ] 
+if [ $1 == -help ] 
 then
-echo "Usage: ton-machine [command]"
-echo "
-Commands:
-	-net <mainnet/testnet>
-	-api <API>
-	-key <JSON seed-file path>
-	-collection <JSON collection file>
-	-deploy -- Mint NFT my_collection
-	-usage -- Using an already created collection
+	echo "Usage: ton-machine [command]"
+	echo "
+	Commands:
+		-net <mainnet/testnet>
+		-api <API>
+		-key <JSON seed-file path>
+		-collection <JSON collection file>
+		-deploy -- Mint NFT my_collection
+		-usage -- Using an already created collection
 
-Example:
-	./ton-machine.sh -net testnet -api 0000000000000000000000000000000000000000000000000000000000000000 -key ./key.json -collection ./jsonCollection.json -deploy
-	./ton-machine.sh -net mainnet -api 0000000000000000000000000000000000000000000000000000000000000000 -key ./key.json -collection ./jsonCollection.json -usage
-	"
-
+	Example:
+		./ton-machine.sh -net testnet -api 0000000000000000000000000000000000000000000000000000000000000000 -key ./key.json -collection ./jsonCollection.json -deploy
+		./ton-machine.sh -net mainnet -api 0000000000000000000000000000000000000000000000000000000000000000 -key ./key.json -collection ./jsonCollection.json -usage
+		"
+	cmd /k
 fi
 
 if [[ $9 == -deploy ]]
@@ -35,11 +36,4 @@ fi
 if [[ $9 == -usage ]]
 then
 	node lib/usage_collection.js $2 $4 $6 $8
-fi
-
-fi
-
-if [[ $3 == -usage]]
-then
-
 fi
