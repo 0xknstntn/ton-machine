d31f7865741c70c6755caf8025769f52ad9f36e2
mnstr.asuoki <konstantinjogotsen@gmail.com>
2022-06-08 22:22:32 +0500

    Added Mint Jetton (Analogue of ERC20 in the TON blockchain)

--- ton-machine.sh
+++ ton-machine.sh
@@ -22,18 +22,24 @@ then
 		-usage -- Using an already created collection
 
 	Example:
-		./ton-machine.sh -net testnet -api 0000000000000000000000000000000000000000000000000000000000000000 -key ./key.json -collection ./jsonCollection.json -deploy
-		./ton-machine.sh -net mainnet -api 0000000000000000000000000000000000000000000000000000000000000000 -key ./key.json -collection ./jsonCollection.json -usage
+		./ton-machine.sh -net testnet -api 0000000000000000000000000000000000000000000000000000000000000000 -key ./key.json -collection ./jsonCollection.json -deploy_NFT
+		./ton-machine.sh -net mainnet -api 0000000000000000000000000000000000000000000000000000000000000000 -key ./key.json -collection ./jsonCollection.json -usage_NFT
+		./ton-machine.sh -net testnet -api 0000000000000000000000000000000000000000000000000000000000000000 -key ./key.json -collection ./jsonCollection.json -deploy_jetton
 		"
 	cmd /k
 fi
 
-if [[ $9 == -deploy ]]
+if [[ $9 == -deploy_NFT ]]
 then
 	node lib/deploy_collection.js $2 $4 $6 $8
 fi
 
-if [[ $9 == -usage ]]
+if [[ $9 == -usage_NFT ]]
 then
 	node lib/usage_collection.js $2 $4 $6 $8
+fi
+
+if [[ $9 == -deploy_jetton ]]
+then
+	node lib/deploy_jetton.js $2 $4 $6 $8
 fi
